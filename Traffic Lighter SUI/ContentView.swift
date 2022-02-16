0c2b17e2e94d63d773e348ab48b97b6b585a1857
anvar0522 <anvargehe@gmail.com>
2022-02-16 18:26:47 +0500

    Initial Commit

---
+++ Traffic Lighter SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic Lighter SUI
+//
+//  Created by anvar on 16/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Traffic Lighter SUI/Traffic_Lighter_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Lighter_SUIApp.swift
+//  Traffic Lighter SUI
+//
+//  Created by anvar on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Lighter_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
